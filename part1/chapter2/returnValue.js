9cd8c0009a90a112d8f49fe250ec41864386b746
Hoa0304 <tranthicamhoa2k4@gmail.com>
2023-11-29 15:20:29 +0700

    control flow and return value (#2)

---
+++ part1/chapter2/returnValue.js
@@ -0,0 +1,11 @@
+function calculateSum(a, b) {
+    return a + b;
+}
+  
+var result = calculateSum(3, 5);
+console.log(result); // 8
+
+console.log(Math.max(2, 4));
+// 4
+console.log(Math.min(2, 4) + 100);
+// 102
