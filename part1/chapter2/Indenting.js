f4267a828831f106f356697a5f75c4b0e4cc51df
Hoa0304 <tranthicamhoa2k4@gmail.com>
2023-11-30 10:55:02 +0700

    indenting code (#2)

---
+++ part1/chapter2/Indenting.js
@@ -0,0 +1,14 @@
+if (false != true) {
+    console.log("That makes sense.");
+    if (1 < 2) {
+    console.log("No surprise there.");
+    }
+}
+
+function greet(name) {
+    if (name) {
+      console.log("Hello, " + name + "!");
+    } else {
+      console.log("Hello, stranger!");
+    }
+}
