e62cbad625a5f7ec724bf4dc1277f638a49331be
Hoa0304 <tranthicamhoa2k4@gmail.com>
2023-12-01 18:03:13 +0700

    function (#3)

---
+++ part1/chapter3/function.js
@@ -0,0 +1,6 @@
+function greet(name) {
+    return "Hello, " + name + "!";
+  }
+  
+  // Call the function and print the result to the console
+console.log(greet("John")); // Output: "Hello, John!"
