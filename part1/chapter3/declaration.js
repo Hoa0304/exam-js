1833e66e0f225ff414732230ccde11a6b145c612
Hoa0304 <tranthicamhoa2k4@gmail.com>
2023-12-03 16:27:37 +0700

    declaeation

---
+++ part1/chapter3/declaration.js
@@ -0,0 +1,11 @@
+function sayHello(name) {
+    console.log("Hello, " + name + "!");
+}
+ 
+sayHello("Hoa"); // Output: "Hello, Hoa!"
+
+const sayHello = function(name) {
+    console.log("Hello, " + name + "!");
+}
+ 
+sayHello("Dat"); // Output: "Hello, Dat!"
